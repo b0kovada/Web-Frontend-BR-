931be0e10dccdad2b9e05ca9c82e76cbbfc9477f
b0kovada <b0kovada@vasvari.org>
2025-01-10 09:00:21 +0100

    stiluslap

--- ToDOApp/src/main.js
+++ ToDOApp/src/main.js
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-//import './assets/styles.scss';
+import './assets/styles.css';
 
 const app = createApp(App)
 
